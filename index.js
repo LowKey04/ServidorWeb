70257992895b2a18f9b1096962e8b91a008dbab9
Mota <sa.torresmota@ugto.mx>
2023-08-21 08:56:41 -0600

    se uso html y views}

--- index.js
+++ index.js
@@ -1,10 +1,29 @@
-const http = require('http')
-const port = 5000
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 5001
 
 
-const server = http.createServer((req, res) => {
-    res.end('Hola alumnos')
+//app.use(express.static(__dirname + '/public'))
+
+//motor de plantillas o vistas
+app.set('view engine', 'ejs')
+app.set('vies', __dirname + '/views ')
+
+app.get('/',(req, res) => {
+    res.render('index', {titulo: 'Index con EJS', parametro: 'holis'})
 })
- server.listen(port, () =>{
-    console.log('arranca el servidor')
+
+app.get('/lenguajes',(req, res) => {
+    res.sendFile(__dirname + '/public/index.html')
+})
+
+//const server = http.createServer((req, res) => {
+//    res.end('Hola alumnos')
+//})
+ 
+
+app.listen(port, () =>{
+    //Las dos sentencias de console.log son correctas
+    //console.log('arranca el servidor', port)
+    console.log([`Arranca el servidor ${port}`])
  })
